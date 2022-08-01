b6c3d0ab2fb8cd846f0b3ac8901e69fa094207c1
palkyinc <migvicpereyra@gmail.com>
2022-08-01 17:01:47 -0300

    correcciones al listadoContratros

--- app/Http/Controllers/ContratoController.php
+++ app/Http/Controllers/ContratoController.php
@@ -96,10 +96,17 @@ class ContratoController extends Controller
         date_default_timezone_set(Config::get('constants.USO_HORARIO_ARG'));
         $contratos = Contrato::where('no_paga', false)->where('baja', false)->get();
         $newFile = fopen ('../storage/app/public/ListadoClientes-' . date('Ymd') . '.csv', 'w');
-        fwrite($newFile ,'ID Genesys;APELLIDO, Nombre;Plan;Estado;Sistema' . PHP_EOL);
+        fwrite($newFile ,'ID Genesys;APELLIDO, Nombre;Plan;Estado;Sistema;Comentarios' . PHP_EOL);
         foreach ($contratos as $key => $value)
         {
-            fwrite($newFile , $value->relCliente->id . ';' . $value->relCliente->getNomyApe() . ';' . $value->relPlan->nombre . ';' . ($value->activo ? 'Habilitado' : 'Deshabilitado') . ';SLAM' . PHP_EOL);
+            $pruebaVelocidad = Issue::where('closed', false)->where('titulo_id', 4)->where('contrato_id', $value->id)->first();
+            fwrite($newFile ,   $value->relCliente->id . ';' . 
+                                $value->relCliente->getNomyApe() . ';' . 
+                                $value->relPlan->nombre . ';' . 
+                                ($value->activo ? 'Habilitado' : 'Deshabilitado') .  
+                                ';SLAM' . ';' .
+                                ($pruebaVelocidad ? 'Con Prueba de Velocidad' : '') . 
+                                PHP_EOL);
         }
         fclose($newFile);
         return Storage::disk('public')->download('ListadoClientes-' . date('Ymd') . '.csv');
